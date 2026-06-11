33e87a3c3510dec3b9663baedaa1fbb5ad7d8ec8
smallbike <15797642529@163.com>
2026-06-11 08:22:32 +0800

    优化

--- yuanrenxue/python_spider/18 - 验证码 - 图片验证码-计算/fetch_18.py
+++ yuanrenxue/python_spider/18 - 验证码 - 图片验证码-计算/fetch_18.py
@@ -5,34 +5,41 @@ from pathlib import Path
 import requests
 from inferring import ImageModel
 
+
 headers = {
     "accept": "application/json, text/javascript, */*; q=0.01",
-    "accept-language": "zh-CN,zh;q=0.9",
-    "cache-control": "no-cache",
+    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,fil;q=0.7",
     "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
     "origin": "https://www.python-spider.com",
-    "pragma": "no-cache",
-    "priority": "u=0, i",
+    "priority": "u=1, i",
     "referer": "https://www.python-spider.com/challenge/18",
-    "sec-ch-ua": "\"Chromium\";v=\"148\", \"Google Chrome\";v=\"148\", \"Not/A)Brand\";v=\"99\"",
+    "sec-ch-ua": "\"Google Chrome\";v=\"149\", \"Chromium\";v=\"149\", \"Not)A;Brand\";v=\"24\"",
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"Windows\"",
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36",
     "x-requested-with": "XMLHttpRequest"
 }
 cookies = {
-    "sessionid": "6w7zvtm12ilqv71k1r3tepo1i3to8vk1"
-
+    "sessionid": "6w7zvtm12ilqv71k1r3tepo1i3to8vk1",
+    "no-alert": "true"
 }
 
-def total_number(page):
-    data = {
-        'page': page,
-    }
 
+def total_number(page):
+    if page > 1:
+        code_num = challenge18_verify(verfy_)
+        data = {
+            'page': page,
+            'code': str(code_num)
+        }
+    else:
+        data = {
+            'page': page
+        }
+    print(f'data => {data}')
     response = requests.post('https://www.python-spider.com/api/challenge18', headers=headers, cookies=cookies,
                              data=data).json()
     print(f'response  ==>{response}')
@@ -45,8 +52,6 @@ def total_number(page):
         page_num += int(num)
     print(f'第 {page} 页的 数字总和为： {page_num} ===> {num_list}')
     return page_num
-
-
 
 
 def challenge18_verify(verfy_):
@@ -74,10 +79,6 @@ def challenge18_verify(verfy_):
     return resp
 
 
-
-
-
-
 if __name__ == '__main__':
     verfy_ = ImageModel()
 
@@ -85,9 +86,6 @@ if __name__ == '__main__':
     for i in range(1, 101):
         resp = total_number(page=i)
         all_num += resp
-        num = challenge18_verify(verfy_)
-
         time.sleep(1)
 
     print(f'累加的总和为： {all_num}')
-
